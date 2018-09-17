289d62e92bfdf39bbbefcf90d93f788207df1a95
emerson20 <league@iMac-6.attlocal.net>
2018-09-16 17:24:57 -0700

    Code4Life and EvilPolygonsChallenge

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -10,17 +10,29 @@ public class EvilPolygonsChallenge {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
-
+Robot Steve = new Robot();
 		//2. Set the speed to 100
-
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
+Steve.setSpeed(100);
+		int colorChoice=JOptionPane.showOptionDialog(null, "Pick a color.", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"turquoise","green","yellow"}, 0);
+		System.out.println(colorChoice);
 		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
+		Steve.penDown();
+		if(colorChoice == 0) { 
+		Steve.setPenColor(26, 239, 232);
+		}else if(colorChoice == 1) {
+			Steve.setPenColor(37, 239, 26);
+		}else if(colorChoice == 2) {
+			Steve.setPenColor(242, 255, 0);
+		}
 		//4. Ask the use how many polygons they want to be drawn.
-		
+		String bee = JOptionPane.showInputDialog("How many polygons do you want to be drawn?");
 		//5. Use the robot to draw the number of polygons the user requested.
-		
+		int num = Integer. parseInt(bee);
+		for (int i = 0; i < num; i++) {
+Steve.move(200);		
+Steve.turn(90);
+		}
+	
 		//6. Make it so your shapes do not overlap
 
 		//7. Challenge: add more colors to the Option Dialog.
